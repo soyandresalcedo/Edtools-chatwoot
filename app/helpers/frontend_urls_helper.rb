7709f075aeec72f5aa111f896ee060046fee3400
Andrés Salcedo <77406304+soyandresalcedo@users.noreply.github.com>
2025-07-13 20:54:45 -0500

    Fix frontend_url helper to use FRONTEND_URL env var directly

--- app/helpers/frontend_urls_helper.rb
+++ app/helpers/frontend_urls_helper.rb
@@ -1,6 +1,7 @@
 module FrontendUrlsHelper
   def frontend_url(path, **query_params)
     url_params = query_params.blank? ? '' : "?#{query_params.to_query}"
-    "#{root_url}app/#{path}#{url_params}"
+    base_url = ENV['FRONTEND_URL'].present? ? "https://#{ENV['FRONTEND_URL']}" : root_url
+    "#{base_url}/app/#{path}#{url_params}"
   end
 end
