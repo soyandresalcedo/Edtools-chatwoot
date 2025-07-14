63773a7ea56811f5a8bf2dd1cf4bf5d3c2c02204
Andrés Salcedo <77406304+soyandresalcedo@users.noreply.github.com>
2025-07-13 19:52:26 -0500

    Fix action_mailer default_url_options for email generation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,6 @@ Rails.application.configure do
   # :sendgrid for Sendgrid
   config.action_mailbox.ingress = ENV.fetch('RAILS_INBOUND_EMAIL_SERVICE', 'relay').to_sym
 
+  config.action_mailer.default_url_options = { host: ENV.fetch('FRONTEND_URL'), protocol: 'https' }
   Rails.application.routes.default_url_options = { host: ENV['FRONTEND_URL'] }
 end
