1d233f9647662d08d57164c089f9c594df25047e
Andrés Salcedo <77406304+soyandresalcedo@users.noreply.github.com>
2025-07-13 21:06:37 -0500

    Use ENV.fetch for FRONTEND_URL to force error if missing

--- app/helpers/frontend_urls_helper.rb
+++ app/helpers/frontend_urls_helper.rb
@@ -1,7 +1,6 @@
 module FrontendUrlsHelper
   def frontend_url(path, **query_params)
     url_params = query_params.blank? ? '' : "?#{query_params.to_query}"
-    base_url = ENV['FRONTEND_URL'].present? ? "https://#{ENV['FRONTEND_URL']}" : root_url
-    "#{base_url}/app/#{path}#{url_params}"
+    "https://#{ENV.fetch('FRONTEND_URL')}/app/#{path}#{url_params}"
   end
 end
