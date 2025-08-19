4631365830b417475937de6cbc9c8a4506ad477b
Andrés Salcedo <77406304+soyandresalcedo@users.noreply.github.com>
2025-08-18 19:45:21 -0500

    Cambios de textos y enlaces
    Cambios de textos y enlaces

--- app/javascript/v3/views/auth/signup/components/Signup/Form.vue
+++ app/javascript/v3/views/auth/signup/components/Signup/Form.vue
@@ -68,9 +68,9 @@ export default {
     ...mapGetters({ globalConfig: 'globalConfig/get' }),
     termsLink() {
       return this.$t('REGISTER.TERMS_ACCEPT')
-        .replace('https://www.chatwoot.com/terms', this.globalConfig.termsURL)
+        .replace('https://www.edtools.co/terms', this.globalConfig.termsURL)
         .replace(
-          'https://www.chatwoot.com/privacy-policy',
+          'https://www.edtools.co/politica-de-privacidad',
           this.globalConfig.privacyURL
         );
     },
